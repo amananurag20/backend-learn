aa3327d6b0ebeb5b08dd81427dedd063018e206e
amananurag <amananurag.20@gmail.com>
2025-07-16 10:17:30 +0530

    added post request

--- index.js
+++ index.js
@@ -1,56 +1,59 @@
-const express= require("express");
-const mongoose=require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/User");
 
-const app= express();
+const app = express();
 
-const dbConnect=async()=>{
+app.use(express.json()); //json->js object
+app.use(express.urlencoded({ extended: false }));
 
-    try{
-        await mongoose.connect("mongodb+srv://amananurag20:ZBdNKY4UUtAcciyt@cluster0.5prn1dy.mongodb.net/");
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://amananurag20:ZBdNKY4UUtAcciyt@cluster0.5prn1dy.mongodb.net/studyplanner"
+    );
 
-        console.log("database connected");
+    console.log("database connected");
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-    }catch(e){
-        console.log(e);
-    }
-}
+dbConnect();
 
-dbConnect()
+app.get("/user", (req, res) => {
+  console.log("routeeee get");
+  console.log(req.query);
+  res.send("hello world");
+});
 
-const m1=(req,res,next)=>{
+app.post("/user", async(req, res) => {
+  const { name, password, email } = req.body;
+
+  try{
+      const user= await User.create({
+        name,
+        email,
+        password,   
+      });
     
-    req.game=["cricket","football","hockey"];
-    console.log("m1 called");   
-    next();
-   
-}
+      res.status(201).json({success:true,user})
+  }catch(e){
+    console.log(e);
+    res.status(400).json({success:false,message:"something went wrong"})
+  } 
+     
 
-const m2=(req,res,next)=>{
-    console.log("m2 called");    
-    next();
-}
-
-app.get("/user",m1,m2,(req,res)=>{
-    console.log("After m1")
-    console.log(req.game)
-    res.send("hello world");
-
-});     
-
-app.post("/user",(req,res)=>{
-    res.send("post request");
 });
 
-app.delete("/user",(req,res)=>{
-    res.send("delete request");
+app.delete("/user", (req, res) => {
+  res.send("delete request");
 });
 
-app.patch("/user",(req,res)=>{
-    res.send("patch request");
+app.patch("/user", (req, res) => {
+  res.send("patch request");
 });
 
-app.listen(5000,()=>{
-    console.log("server is listening on port 5000")
-})
-
-
+app.listen(5000, () => {
+  console.log("server is listening on port 5000");
+});
