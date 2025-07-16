c5b5c2facecc515e1a1eeded5c9adb448d6345b8
amananurag <amananurag.20@gmail.com>
2025-07-16 11:10:06 +0530

    added get request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt= require("bcrypt");
 const User = require("./models/User");
 
 const app = express();
@@ -21,20 +22,39 @@ const dbConnect = async () => {
 
 dbConnect();
 
-app.get("/user", (req, res) => {
-  console.log("routeeee get");
-  console.log(req.query);
-  res.send("hello world");
+app.get("/user", async(req, res) => {
+   
+    try{
+        const users= await User.find();
+        res.status(200).json({success:true,users})
+    }catch(e){
+       console.log(e);
+       res.status(400).json({success:false,message:"something went wrong"}) 
+    }
 });
 
 app.post("/user", async(req, res) => {
   const { name, password, email } = req.body;
 
+  if(!password || !email){
+      res.json({success:false,message:"password and email are required"})
+  }
+
+  const user=await User.find({email:email});
+
+  if(user){
+    return res.json({success:false,message:"user already exists"});
+  }
+
+
+  const hashPassword= await bcrypt.hash(password,10);
+  console.log(hashPassword);
+
   try{
       const user= await User.create({
         name,
         email,
-        password,   
+        password:hashPassword,   
       });
     
       res.status(201).json({success:true,user})
