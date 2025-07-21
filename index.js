439586330ca97d4fbf639d44efd135658cf6bfe4
amananurag <amananurag.20@gmail.com>
2025-07-21 10:08:15 +0530

    header based auth

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt= require("bcrypt");
+const bcrypt = require("bcrypt");
 const User = require("./models/User");
-const jwt= require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const Movie = require("./models/Movie");
-
+const verifyToken = require("./middleware/verifyToken");
 
 const app = express();
 
@@ -25,164 +25,142 @@ const dbConnect = async () => {
 
 dbConnect();
 
-app.get("/user", async(req, res) => {
-   
-    try{
-        const users= await User.find();
-        res.status(200).json({success:true,users})
-    }catch(e){
-       console.log(e);
-       res.status(400).json({success:false,message:"something went wrong"}) 
-    }
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ success: true, users });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ success: false, message: "something went wrong" });
+  }
 });
 
-app.get("/user/:id", async(req, res) => {
-   
-    try{
-        const user= await User.findById(req.params.id);
-        res.status(200).json({success:true,user})
-    }catch(e){
-       console.log(e);
-       res.status(400).json({success:false,message:"something went wrong"}) 
-    }
+app.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({ success: true, user });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ success: false, message: "something went wrong" });
+  }
 });
 
-app.post("/user/signup", async(req, res) => {
+app.post("/user/signup", async (req, res) => {
   const { name, password, email } = req.body;
 
-  if(!password || !email){
-      res.json({success:false,message:"password and email are required"})
+  if (!password || !email) {
+    res.json({ success: false, message: "password and email are required" });
   }
 
-  const user=await User.find({email:email});
+  const user = await User.find({ email: email });
 
-  if(user){
-    return res.json({success:false,message:"user already exists"});
+  if (user) {
+    return res.json({ success: false, message: "user already exists" });
   }
 
-
-  const hashPassword= await bcrypt.hash(password,10);
+  const hashPassword = await bcrypt.hash(password, 10);
   console.log(hashPassword);
 
-  try{
-      const user= await User.create({
-        name,
-        email,
-        password:hashPassword,   
-      });
-    
-      res.status(201).json({success:true,user})
-  }catch(e){
-    console.log(e);
-    res.status(400).json({success:false,message:"something went wrong"})
-  } 
-     
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
 
+    res.status(201).json({ success: true, user });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ success: false, message: "something went wrong" });
+  }
 });
 
-app.post("/user/login", async(req, res) => {
-
+app.post("/user/login", async (req, res) => {
   const { password, email } = req.body;
-  
 
-
-  if(!password || !email){
-    return res.json({success:false,message:"password and email are required"})
+  if (!password || !email) {
+    return res.json({
+      success: false,
+      message: "password and email are required",
+    });
   }
 
-  const user=await User.findOne({email:email});
-  console.log(user);
-
-
-  if(!user){
-    return res.json({success:false,message:"user does not exists"});
-  }
+  const user = await User.findOne({ email: email });
   
-  console.log(password,user.password)
-  const isPasswordCorrect= await bcrypt.compare(password,user.password);
 
-  if(!isPasswordCorrect){
-    return res.json({success:false,message:"wrong credentials"});
+  if (!user) {
+    return res.json({ success: false, message: "user does not exists" });
+  }
 
-  };
+  console.log(password, user.password);
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-  const token= jwt.sign({email:user.email,id:user._id,game:"cricket"},"secret",{
-    expiresIn:"10m"
-  })
+  if (!isPasswordCorrect) {
+    return res.json({ success: false, message: "wrong credentials" });
+  }
 
-  res.setHeader("x-movie","abcd")
+  const token = jwt.sign(
+    { email: user.email, id: user._id, game: "cricket" },
+    "secret",
+    {
+      expiresIn: "30m",
+    }
+  );
 
-  res.json({success:true,user,message:"login successful",token});
-})
+  res.setHeader("x-movie", "abcd");
 
+  res.json({ success: true, user, message: "login successful", token });
+});
 
-
-app.delete("/user/:id", async(req, res) => {
-   
-  try{
-    const id=req.params.id;    
-    const user= await User.findByIdAndDelete(id);
-    res.status(200).json({success:true,user});   
-
-  }catch(e){
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findByIdAndDelete(id);
+    res.status(200).json({ success: true, user });
+  } catch (e) {
     console.log(e);
-    res.json({success:false,message:"something went wrong"})
+    res.json({ success: false, message: "something went wrong" });
   }
- 
 });
 
-app.patch("/user/:id", async(req, res) => {
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(req.body);
 
-   try{
-        const id=req.params.id;
-        console.log(req.body)
-    
-        const user= await User.findByIdAndUpdate(id,req.body,{new:true});
-        res.status(200).json({success:true,user})
-    }catch(e){
-       console.log(e);
-       res.status(400).json({success:false,message:"something went wrong"}) 
-    }
-      
+    const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json({ success: true, user });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ success: false, message: "something went wrong" });
+  }
 });
 
-
-app.get("/movie",(req,res)=>{
-    
-   const token= req.headers.authorization.split(" ")[1];
-   console.log(token);   
-
+app.get("/movie", verifyToken,async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.status(200).json({ success: true, movies });
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false, message: "something went wrong" });
+  }
 });
 
+app.post("/movie", async (req, res) => {
+  try {
+    const movie = await Movie.create(req.body);
+    res.status(200).json({ success: true, movie });
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ success: false, message: "something went wrong" });
+  }
+});
 
+app.get("/get-current-user",verifyToken,async(req,res)=>{
 
-app.post("/movie",async (req,res)=>{
-
-  
-    try{
-        const movie= await Movie.create(req.body);
-        res.status(200).json({success:true,movie})
-    }catch(e){
-       console.log(e);
-       res.status(400).json({success:false,message:"something went wrong"});
-
-    }
-
+  const user=await User.findOne({email:req.user.email});
+  res.json({success:true,user})
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
---
+++ middleware/verifyToken.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const verifyToken=(req,res,next)=>{
+    
+    try{
+         const token=req.headers.authorization.split(" ")[1];
+         const data= jwt.verify(token, "secret");     
+         req.user=data;
+         next();
+
+     }catch(e){
+         res.json({success:false,message:"invalid token"})
+     }
+
+};
+
+
+module.exports=verifyToken;
